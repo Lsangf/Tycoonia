ecd77fbc161618fc818e9c868459671e25b944b1
Lsangf <z9333601@gmail.com>
2026-01-14 21:25:27 +0200

    добавлены тесты для проверки изменения количества ресурсов и добычи шахты, (энергия не используется)

---
+++ TycooniaTest/MineOperations.cs
@@ -0,0 +1,26 @@
+﻿using Tycoonia.Application.Mining;
+using Tycoonia.Domain.Buildings.EnergyPlant.Storage;
+using Tycoonia.Domain.Buildings.Mine;
+using Tycoonia.Domain.Resources.RawResources;
+using Tycoonia.Domain.Resources.Storage;
+namespace TycooniaTest
+{
+    public class MineOperations
+    {
+        [Fact]
+        public void MineOperationsExtraction()
+        {
+            StorageResources storageResources = new();
+            MineClay clayMine = new((0, 0));
+            MineCoal coalMine = new((0, 0));
+            List<MineBase> mines = [clayMine, coalMine];
+            EnergyStorage energyStorage = new();
+
+
+            ResourceExtraction.ResourceExtractionMine(storageResources, mines, energyStorage);
+            long resultMiningClay = storageResources.Storage[mines[0].Name];
+
+            Assert.Equal(5, resultMiningClay);
+        }
+    }
+}
