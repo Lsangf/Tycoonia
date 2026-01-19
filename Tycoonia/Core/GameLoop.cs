09128d1573b2bb8f7dde580bc5210e900b0ef9d6
Lsangf <z9333601@gmail.com>
2026-01-19 22:15:26 +0200

    добавлена инициализация объектов, игровой цикл основа

--- Tycoonia/Core/GameLoop.cs
+++ Tycoonia/Core/GameLoop.cs
@@ -1,6 +1,23 @@
-﻿namespace Tycoonia.Core
+﻿using Tycoonia.Domain.Buildings.EnergyPlant.Storage;
+using Tycoonia.Domain.Buildings.EnergyPlant.TPP;
+using Tycoonia.Domain.Buildings.Factory;
+using Tycoonia.Domain.Player;
+using Tycoonia.Domain.Resources.EnergyResources;
+using Tycoonia.Domain.Resources.ProcessedResources;
+using Tycoonia.Domain.Resources.RawResources;
+using Tycoonia.Domain.Resources.Storage;
+
+namespace Tycoonia.Core
 {
-    internal class GameLoop
+    public class GameLoop
     {
+        internal static void StartGameLoop(
+            PlayerReal player, 
+            Clay clay, Coal coal, Bricks bricks, Energy energy, 
+            FactoryBricks factoryBricks, CoalTPP coalTPP, 
+            StorageResources storage, EnergyStorage energyStorage)
+        {
+            
+        }
     }
 }
--- Tycoonia/Core/InitializationCore.cs
+++ Tycoonia/Core/InitializationCore.cs
@@ -1,10 +1,63 @@
-﻿namespace Tycoonia.Core
+﻿using Tycoonia.Domain.Buildings.EnergyPlant.Storage;
+using Tycoonia.Domain.Buildings.EnergyPlant.TPP;
+using Tycoonia.Domain.Buildings.Factory;
+using Tycoonia.Domain.Player;
+using Tycoonia.Domain.Resources.EnergyResources;
+using Tycoonia.Domain.Resources.ProcessedResources;
+using Tycoonia.Domain.Resources.RawResources;
+using Tycoonia.Domain.Resources.Storage;
+
+namespace Tycoonia.Core
 {
     public class InitializationCore
     {
-        static void Main()
+        private static Random random = new();
+        public static void Main()
         {
+            PlayerReal player = CreatePlayer();
 
+            // resources
+            Clay clay = new();
+            Coal coal= new();
+            Bricks bricks = new();
+            Energy energy = new();
+
+            // factories and plants
+            FactoryBricks factoryBricks = new();
+
+            CoalTPP coalTPP = new();
+
+            // storage
+            StorageResources storage = new();
+            EnergyStorage energyStorage = new();
+
+            // gameloop
+            GameLoop.StartGameLoop(
+                player, 
+                clay, coal, bricks, energy, 
+                factoryBricks, coalTPP, 
+                storage, energyStorage);
+        }
+
+        public static PlayerReal CreatePlayer()
+        {
+            byte lengthName = 5;
+            string name = GenerateRandomName(lengthName);
+
+            return new PlayerReal(name, 100000);
+        }
+
+        public static string GenerateRandomName(byte length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+            char[] buffer = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                buffer[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(buffer);
         }
     }
 }
